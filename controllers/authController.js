b3b3b6dfc3185af067ee678626e338f8927c71f8
ZainabMobin <rgbrgb2077@gmail.com>
2025-12-17 10:56:50 +0500

    Move logout function implementation to Controller layer

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,3 +62,18 @@ export async function signup(req, res) {
         return res.status(400).json({ error: err.message });
     }
 }
+export async function logout(req, res){
+  try { 
+    // Clear the token cookie
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // only send over HTTPS in prod
+      sameSite: "strict"
+    });
+
+    res.json({ success: true, message: "Logged out successfully" });
+  } catch (err) {
+    console.error("Logout error:", err);
+    res.status(500).json({ success: false, error: "Failed to log out" });
+  }
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,26 +1,13 @@
 import express from "express";
-import { login, signup } from "../controllers/authController.js";
+import { login, signup, logout} from "../controllers/authController.js";
 
 const router = express.Router();
 
+//login
 router.post("/login", login);
+//signup
 router.post("/signup", signup);
- 
 // Logout route
-router.post("/logout", (req, res) => {
-  try {
-    // Clear the token cookie
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // only send over HTTPS in prod
-      sameSite: "strict"
-    });
-
-    res.json({ success: true, message: "Logged out successfully" });
-  } catch (err) {
-    console.error("Logout error:", err);
-    res.status(500).json({ success: false, error: "Failed to log out" });
-  }
-});
+router.post("/logout", logout);
 
 export default router;
